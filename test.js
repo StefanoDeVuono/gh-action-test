8581bdf52a3c142814c2156e5fc274807482b607
Stefano De Vuono <io@stefanodevuono.com>
2020-03-22 23:55:47 -0700

    no repl set: should fail... Transaction numbers are only allowed on a replica set member or mongos

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose'),
   { to } = require('await-to-js'),
+  { Schema } = mongoose,
   test = require('ava')
 
 const [DB_ADDR, DB_PORT, REPLICA_NAME] = ['localhost', 27017, 'rsTest']
 
-const mongoUri = `mongodb://${DB_ADDR}:${DB_PORT}/test?replSet=${REPLICA_NAME}`,
+const mongoUri = `mongodb://${DB_ADDR}:${DB_PORT}/test`,
   options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -23,4 +24,27 @@ test('model', async t => {
   const [err, res] = await to(kitty.save())
   if (err) t.fail(err)
   t.is(res.name, 'Zildjian', 'model saved')
+})
+
+test('transaction', async t => {
+  const Customer = mongoose.model('Customer', new Schema({ name: String }))
+  await Customer.createCollection()
+  const [_err, session] = await to(mongoose.startSession())
+  session.startTransaction()
+  await Customer.create([{ name: 'Test transaction customer' }], { session })
+  const [err1, noCustomer] = await to(
+    Customer.findOne({ name: 'Test transaction customer' })
+  )
+  t.falsy(noCustomer)
+  const [err2, customerT] = await to(
+    Customer.findOne({ name: 'Test transaction customer' }).session(session)
+  )
+  t.truthy(customerT)
+  await session.commitTransaction()
+  const [err3, customer] = await to(
+    Customer.findOne({
+      name: 'Test transaction customer',
+    })
+  )
+  t.truthy(customer)
 })
